814d1b964485c65abb728d229cca75364adfbb87
Armin Fröhlich <mail@arminfroehlich.de>
2020-11-18 09:44:12 +0100

    A2, add BookList, BookListItem

---
+++ src/types/Book.ts
@@ -0,0 +1,15 @@
+export default interface Book {
+  isbn: string;
+  title: string;
+  authors: string[];
+  published: Date;
+  subtitle?: string;
+  rating?: number;
+  thumbnails: IThumbnail[];
+  description?: string;
+}
+
+export interface IThumbnail {
+  url: string;
+  title?: string;
+}
