24b6a3ad0f6dc8d9ecea8301c598ab2399bc1dc8
Armin Fröhlich <mail@arminfroehlich.de>
2020-11-23 17:10:27 +0100

    A6, optional, add Layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,15 @@
 import React, {ReactElement} from 'react';
-import {BrowserRouter as Router, Redirect, Switch, Route, NavLink} from 'react-router-dom'
+import {Redirect, Switch, Route} from 'react-router-dom'
 
 import BookList from './BookList'
 import BookDetails from './BookDetails'
 import Home from './Home'
+import Layout from './Layout';
 
 export default function App(): ReactElement {
 
   return (
-    <Router>
-      <div className="ui menu">
-        <NavLink to="/home" className="item" activeClassName="active">Home</NavLink>
-        <NavLink to="/books" className="item" activeClassName="active">Books</NavLink>
-      </div>
-
+    <Layout>
       <Switch>
         <Route path='/books/:isbn'>
           <BookDetails />
@@ -31,6 +27,6 @@ export default function App(): ReactElement {
           <Redirect to="/home" />
         </Route>
       </Switch>
-    </Router>
+    </Layout>
   )
 }
