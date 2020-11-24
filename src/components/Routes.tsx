373eb18ddea4cdc16fc0c9379a63a37852216ae1
Armin Fröhlich <mail@arminfroehlich.de>
2020-11-24 10:23:00 +0100

    A6, optional, add Routes

---
+++ src/components/Routes.tsx
@@ -0,0 +1,28 @@
+import React, {ReactElement} from 'react';
+import {Redirect, Switch, Route} from 'react-router-dom'
+
+import BookList from './BookList'
+import BookDetails from './BookDetails'
+import Home from './Home'
+
+export default function Routes(): ReactElement {
+  return (
+    <Switch>
+      <Route path='/books/:isbn'>
+        <BookDetails />
+      </Route>
+
+      <Route path='/books'>
+        <BookList />
+      </Route>
+
+      <Route path='/home'>
+        <Home />
+      </Route>
+
+      <Route exact path=''>
+        <Redirect to="/home" />
+      </Route>
+    </Switch>
+  )
+}
