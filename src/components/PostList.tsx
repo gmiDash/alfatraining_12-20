883b0703fbc78a35280baf026e1bdcbb7d2e3d0f
Armin Fröhlich <mail@arminfroehlich.de>
2020-11-24 14:48:09 +0100

    add axios

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,7 @@
-import React, { ReactElement } from 'react'
-import { posts } from '../shared/posts'
+import React, { ReactElement, useState, useEffect } from 'react'
+import axios, { AxiosResponse } from 'axios'
+
+// import { posts } from '../shared/posts'
 import Post from '../types/Post'
 import PostListItem from './PostListItem'
 
@@ -8,6 +10,15 @@ interface Props {
 }
 
 export default function PostList(props: Props): ReactElement {
+  const [posts, setPosts] = useState<Post[]>()
+
+  useEffect(() => {
+    axios.get<Post[]>('http://jsonplaceholder.typicode.com/posts')
+      .then((resp: AxiosResponse<Post[]>) => setPosts(resp.data))
+  }, [])
+
+  if (!posts) { return <p>Lade Daten</p>}
+
   return (
     <div className="ui cards">
       {
